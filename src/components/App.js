e35ae13483de90d883214f65969b6314596cd543
Hrushi1509 <90896961+Hrushi1509@users.noreply.github.com>
2022-06-07 16:55:44 +0530

    rushi

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h2 data-ns-test="project-name">Web Development</h2>
+      <p data-ns-test="project-description">gyukuggyu khyugtftf uyuyuy</p>
+    </div>
+  );
+};
 
 export default App;
